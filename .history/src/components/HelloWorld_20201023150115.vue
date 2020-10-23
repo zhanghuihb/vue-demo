09e73d2fa6794b5bb07b265561db10feba7610ae
zhanghui <zhanghuihb@126.com>
2020-10-23 16:26:15 +0800

    first commit

---
+++ .history/src/components/HelloWorld_20201023150115.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <span>
+      鼠标悬停几秒钟查看此处动态绑定的提示信息！
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ .history/src/components/HelloWorld_20201023152849.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <!-- <span v-bind:title="msg">
+      鼠标悬停几秒钟查看此处动态绑定的提示信息！
+    </span> -->
+    <p>{{msg}}</p>
+    <button v-on:click="reverseMessage">反转消息</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Hello Vue.js'
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.msg = this.msg.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
